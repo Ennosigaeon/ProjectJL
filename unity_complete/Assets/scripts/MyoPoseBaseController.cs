5239ea728b778e9956e54a577db12138a9ea19d5
Marc <marczoeller@gmx.net>
2016-12-02 21:30:01 +0000

    Add MyoPoseBaseController

---
+++ unity_complete/Assets/scripts/MyoPoseBaseController.cs
@@ -0,0 +1,96 @@
+﻿using UnityEngine;
+using Pose = Thalmic.Myo.Pose;
+using UnlockType = Thalmic.Myo.UnlockType;
+
+public class MyoPoseController : MonoBehaviour {
+
+    public GameObject myoGameObject = null;
+    public float strechFactor = 1f;
+    public float minStrechFactor = 0.5f;
+    public float maxStrechFactor = 2.5f;
+    public KeyCode resetKey = KeyCode.Space;
+
+    public float speed = 1f;
+    public bool updateOnChange = false;
+    public float maxOffset = 2f;
+
+    private ThalmicMyo myo = null;
+    private Pose lastPose = Pose.Unknown;
+
+    // Use this for initialization
+    void Start() {
+        Debug.LogError("Myo Pose Controller start");
+        myo = myoGameObject.GetComponent<ThalmicMyo>();
+        if (myo == null) {
+            myo.Unlock(UnlockType.Hold);
+        }
+    }
+
+    void OnApplicationQuit() {
+        Debug.Log("Resetting unlock behaviour.");
+        myo.Unlock(UnlockType.Timed);
+    }
+
+    // Update is called once per frame
+    void Update() {
+        if (Input.GetKey(resetKey))
+            reset();
+
+        Pose pose = myo == null ? Pose.Unknown : myo.pose;
+        if (pose == lastPose && updateOnChange)
+            return;
+
+        if (pose != lastPose)
+            Debug.LogError("Pose: " + pose.ToString());
+        lastPose = pose;
+
+        switch (pose) {
+            case Pose.DoubleTap:
+                onDoubleTap();
+                break;
+            case Pose.FingersSpread:
+                onFingerSpread();
+                break;
+            case Pose.Fist:
+                onFist();
+                break;
+            case Pose.Rest:
+                onRest();
+                break;
+            case Pose.Unknown:
+                onUnknown();
+                break;
+            case Pose.WaveIn:
+                onWaveIn();
+                break;
+            case Pose.WaveOut:
+                onWaveOut();
+                break;
+        }
+    }
+
+    protected void reset() {
+    }
+
+    protected void onDoubleTap() {
+    }
+
+    protected void onFingerSpread() {
+    }
+
+    protected void onFist() {
+    }
+
+    protected void onRest() {
+    }
+
+    protected void onUnknown() {
+    }
+
+    protected void onWaveIn() {
+    }
+
+    protected void onWaveOut() {
+    }
+
+}
