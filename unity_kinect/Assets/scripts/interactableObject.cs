2366739e61c3a258408471950e6bd959e87509b6
Christoph Rauterberg <c.rauterberg@stud.uni-goettingen.de>
2016-12-03 03:01:44 +0100

    copied complete to kinect for easier side by side coding

---
+++ unity_kinect/Assets/scripts/interactableObject.cs
@@ -0,0 +1,119 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class InteractableObject : MonoBehaviour
+{
+	public GameObject body = null;
+    private bool isTouched = false;
+
+    void OnEnable()
+    {
+        MyoPoseController.onDoubleTap += doOnDoubleTap;
+        MyoPoseController.onFingerSpread += doOnFingerSpread;
+        MyoPoseController.onFist += doOnFist;
+        MyoPoseController.onRest += doOnRest;
+        MyoPoseController.onWaveIn += doOnWaveIn;
+        MyoPoseController.onWaveOut += doOnWaveOut;
+    }
+
+    void doOnDoubleTap(GameObject body)
+    {
+        Debug.Log("DoubleTab detected with " + this.gameObject);
+
+    }
+
+    void doOnFingerSpread(GameObject body)
+    {
+        Debug.Log("FingerSpread detected with " + this.gameObject);
+        this.gameObject.transform.SetParent(null);
+		this.gameObject.GetComponent<Rigidbody> ().isKinematic = false;
+
+		tossObject (this.gameObject.GetComponent<Rigidbody> ());
+        //		this.attachedRigidbody.isKinematic = false;
+        //TODO body als richtiges Argument uebergeben
+        //		tossObject(this.attachedRigidbody);
+    }
+
+    void doOnFist(GameObject body)
+    {
+        Debug.Log("Fist detected with " + this.gameObject);
+        if (isTouched)
+        {
+			this.gameObject.GetComponent<Rigidbody> ().isKinematic = true;
+			this.gameObject.transform.SetParent(body.gameObject.transform);
+            //			this.attachedRigidbody.isKinematic = true;
+            //			this.gameObject.transform.SetParent(body.gameObject.transform);
+            //TODO hier Element neu erzeugen oder durch andere Geste?
+            Instantiate(this.gameObject);
+        }
+    }
+
+    void doOnRest(GameObject body)
+    {
+        Debug.Log("Rest detected with " + this.gameObject);
+
+    }
+
+    void doOnWaveIn(GameObject body)
+    {
+        Debug.Log("WaveIn detected with " + this.gameObject);
+
+    }
+
+    void doOnWaveOut(GameObject body)
+    {
+        Debug.Log("WaveOut detected with " + this.gameObject);
+
+    }
+
+
+	void OnCollisionEnter(Collision other){
+		if (other.gameObject == body) {
+			isTouched = true;
+		}
+	}
+
+	void OnCollisionExit(Collision other){
+		if (other.gameObject == body) {
+			isTouched = false;
+		}
+	}
+
+    /*	void OnTriggerStay(Collider col)
+        {
+            Debug.Log("Collision with " + col.name + " and activated OnTriggerStay");
+            //Signal for closing hand
+            if (device.GetTouch(SteamVR_Controller.ButtonMask.Trigger))
+            {
+                Debug.Log("Collision with " + col.name + " while Grab-Signal detected");
+                col.attachedRigidbody.isKinematic = true;
+                col.gameObject.transform.SetParent(this.gameObject.transform);
+                Instantiate(this.gameObject);
+            }
+
+            //Signal for opening hand
+            if(device.GetTouchUp(SteamVR_Controller.ButtonMask.Trigger))
+            {
+                Debug.Log("Hand opened, " + col.name + " is thrown");
+                col.gameObject.transform.SetParent(null);
+                col.attachedRigidbody.isKinematic = false;
+
+                tossObject(col.attachedRigidbody);
+            }
+        }
+    */
+
+    private void tossObject(Rigidbody rigidbody)
+    {
+        /*		Transform origin = parent.origin ? parent.origin : parent.transform.parent;
+                if(origin != null)
+                {
+                    rigidbody.velocity = origin.TransformVector(device.velocity);
+                    rigidbody.angularVelocity = origin.TransformVector(device.angularVelocity);
+                } else
+                {
+                    rigidbody.velocity = device.velocity;
+                    rigidbody.angularVelocity = device.angularVelocity;
+                }*/
+    }
+}
